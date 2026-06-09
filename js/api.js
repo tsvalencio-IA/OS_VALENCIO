10ad289102ec03a97484d86e4ecabc7ac8ea0d18
tsvalencio-IA <tsvalencio@gmail.com>
2026-06-09 15:58:34 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@
   Este arquivo apenas separa a chave do config.js, como solicitado, para organizar/disfarçar no MVP.
 */
 window.OS_API = {
-  _k: ["gsk_aVG", "HmvTHAn", "zLcjl7H", "VO1WGd", "yb3FY", "vupCmu", "nVa5jltXBD3pYYkgw4"],
+  _k: ["gsk_YcM7Gg", "J5J3DR", "7BG4Pcv", "cWGdyb3", "FYw1tJ", "oUjjSDBU", "4Zfmv2AA0o1c"],
   getGroqKey(){
     return this._k.join('');
   }
